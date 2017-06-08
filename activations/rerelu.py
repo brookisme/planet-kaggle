a03113574fbead461a089158dfde2985ab099ae1
Brookie Guzder-Williams <brook.williams@gmail.com>
2017-06-08 17:15:48 +0000

    rerelu: first pass

---
+++ activations/rerelu.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import keras
+from keras.models import Sequential
+from keras import layers
+from keras.datasets import mnist
+from keras import backend as K
+
+
+class ReReLu(layers.Layer):
+    ''' A Relu-like activation function for binary output
+                 ___
+        ie:  ___/
+    '''
+    def __init__(self,slope=1,x_intercept=0,**kwargs):
+        self.slope=slope
+        self.x_intercept=x_intercept
+        super().__init__(**kwargs)
+
+
+    def call(self,inputs):
+        inputs=K.expand_dims(inputs, axis=0)
+        zeros=K.zeros_like(inputs)
+        ones=K.ones_like(inputs)
+        shape=K.shape(inputs)
+        relu=K.max(_bound_cat(zeros,inputs),axis=0)
+        rerelu=K.min(_bound_cat(ones,relu,shape),axis=0)
+        return rerelu
+
+
+    #
+    # INTERNAL
+    #
+    def _bound_cat(self,bounds,inputs,shape=None):
+        if shape is not None: inputs=K.reshape(inputs,shape)
+        return K.concatenate([bounds,inputs],axis=0)
