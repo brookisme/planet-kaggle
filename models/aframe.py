5e902fd1946bf9eb55df244671d6fd2c5a45851e
Brookie Guzder-Williams <brook.williams@gmail.com>
2017-06-08 15:59:06 +0000

    flex: output/conv/fc_activation

--- models/aframe.py
+++ models/aframe.py
@@ -12,34 +12,44 @@ import numpy as np
 from models.base import MODEL_BASE
 from activations.stepfunc import Stepfunc
 
+
+
+#
+# DEFAULTS
+#
+DEFAULT_CONV_ACTIVATION='relu'
+DEFAULT_FC_ACTIVATION='relu'
+DEFAULT_OUTPUT_ACTIVATION='sigmoid'
 DEFAULT_CONV_LAYERS=[
     (32,[3]),
     (64,[3]),
     (16,[3])]
-
-
 DEFAULT_FC_LAYERS=[
     256,
     512]
 
 
+
 #
-# COMMON BLOCKS
+# FLEX MODEL
 #
-
-
-
 class Flex(MODEL_BASE):
     def __init__(self,
             lmbda=None,
             batch_norm=False,
             conv_layers=DEFAULT_CONV_LAYERS,
             fc_layers=DEFAULT_FC_LAYERS,
+            conv_activation=DEFAULT_CONV_ACTIVATION,
+            fc_activation=DEFAULT_FC_ACTIVATION,
+            output_activation=DEFAULT_OUTPUT_ACTIVATION,
             **kwargs):
         self.lmbda=lmbda
         self.batch_norm=batch_norm
         self.conv_layers=conv_layers
         self.fc_layers=fc_layers
+        self.conv_activation=conv_activation
+        self.fc_activation=fc_activation
+        self.output_activation=output_activation
         super().__init__(**kwargs)
 
 
@@ -56,7 +66,7 @@ class Flex(MODEL_BASE):
             x=Flatten()(x)
             for output_dim in self.fc_layers:
                 x=self._fc_block(x,output_dim=output_dim)
-            outputs=Dense(self.target_dim, activation='sigmoid')(x)
+            outputs=Dense(self.target_dim, activation=self.output_activation)(x)
             self._model=Model(inputs=inputs,outputs=outputs)
             if self.auto_compile: self.compile()
         return self._model
@@ -69,14 +79,14 @@ class Flex(MODEL_BASE):
     def _conv_block(self,x,filters,layers=[3],pool_size=(2,2)):
         for size in layers:
             x=ZeroPadding2D((1, 1))(x)
-            x=Conv2D(filters, (size,size), activation='relu')(x)
+            x=Conv2D(filters, (size,size), activation=self.conv_activation)(x)
         if self.batch_norm: x=BatchNormalization()(x)
         x=MaxPooling2D(pool_size=pool_size)(x)
         return x
 
 
     def _fc_block(self,x,output_dim=256,dr=0.5):
-        x=Dense(output_dim, activation='relu')(x)
+        x=Dense(output_dim, activation=self.fc_activation)(x)
         if self.batch_norm: x=BatchNormalization()(x)
         x=Dropout(dr)(x)
         return x
