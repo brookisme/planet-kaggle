b1695bd14d71b5adc3ac0de0f4a98722d7e48595
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-06-11 11:47:06 -0700

    first pass at conv_layers test script

--- scripts/conv_test.py
+++ scripts/conv_test.py
@@ -1,5 +1,6 @@
 import os,sys
 sys.path.append(os.environ.get('PKR'))
+import argparse
 import math
 import numpy as np
 from skimage import io
@@ -10,61 +11,167 @@ from helpers.planet import PlanetData
 from helpers.dfgen import DFGen
 import utils
 
+#
+# CONFIG
+#
+VALID_PCT=0.20
+MODEL=af.Flex
+DEFAULT_BATCH_NORM=True
+INITIAL_LR=0.02
+METRICS=['accuracy',utils.k_f2]
+FC_LAYERS=[256,512]
+""" PRERUN """
+PRERUN_LR=0.001
+PRERUN_TRAIN_SIZE=500
+PRERUN_VALID_SIZE=100
+PRERUN_BATCH_SIZE=64
+PRERUN_EPOCHS=1
+""" DEFAULT_CONFIG """
+DEFUALT_TEST_MODE=True
+DEFAULT_TRAIN_SIZE=2000
+DEFAULT_BATCH_SIZE=64
+DEFAULT_EPOCH_PCT=6 
+""" TEST CONFIG """
+TEST_TRAIN_SIZE=128
+TEST_VALID_SIZE=64
+TEST_BATCH_SIZE=32
+TEST_EPOCHS=2
 
-# pld=PlanetData(create=True,train_size='FULL')
-pld=PlanetData(create=True,train_size=500)
-# pld=PlanetData(train_size=250)
+
 
 #
 # MODEL
 #
-conv_layers=None
-fc_layers=[
-    256,
-    512]
-
-def run(ident,conv_list,n,epochs,lr,sz_multi):
-    #
-    # setup model
-    #
-    conv_layers=[
-        (32,conv_list),
-        (64,[3]),
-        (16,[3])]
-    
-    conv_ident=''.join(map(str,conv_list))
-
-    flex=af.Flex(
+def run(
+        data,
+        conv_layers,
+        train_size,
+        valid_size,
+        batch_size,
+        batch_norm,
+        epochs,
+        weights,
+        test_run):
+    ident=_ident(conv_layers)
+    model_obj=MODEL(
+        batch_norm=batch_norm,
         conv_layers=conv_layers,
-        fc_layers=fc_layers,
-        batch_norm=True,
-        metrics=['accuracy',utils.k_f2])
-    train_sz=sz_multi*32
-    valid_sz=math.floor(train_sz*0.20)
-    flex.compile(optimizer=Adam(lr=lr))
-    #
-    # setup model
-    #
-    for i in range(n):
-        print(f'\n\n{ident}: {i}')
-        flex.fit_gen(
+        fc_layers=FC_LAYERS,
+        metrics=METRICS,
+        auto_compile=False)
+    if test_run:
+        names=f'test-cl-{ident}'
+    else:
+        names=f'cl-{ident}'
+        # pre-run
+        model_obj.compile(optimizer=Adam(lr=PRERUN_LR))
+        model_obj.fit_gen(        
+                pdata=data,
+                epochs=PRERUN_EPOCHS,
+                train_sz=PRERUN_TRAIN_SIZE,
+                valid_sz=PRERUN_VALID_SIZE,
+                batch_size=PRERUN_BATCH_SIZE,
+                history_name=f'pre-{names}',
+                checkpoint_name=f'pre-{names}')
+    # full_run
+    model_obj.compile(optimizer=Adam(lr=INITIAL_LR))
+    model_obj.fit_gen(        
             epochs=epochs,
-            pdata=pld,
-            batch_size=64)
-        if (i%2==0):
-            flex.save_weights(f'flex{conv_ident}_{ident}_{i}')
+            pdata=data,
+            train_sz=train_size,
+            valid_sz=valid_size,
+            batch_size=batch_size,
+            history_name=names,
+            checkpoint_name=names)
+
+
+#
+# INTERNAL
+#
+def _ident(conv_layers):
+    clstr=str(conv_layers)
+    clstr=clstr.replace(' ','').replace('),(','-')
+    clstr=clstr.replace(',[','_').replace(',','.')
+    return re.sub(r'[(\)\[\]]','',clstr)
+
+
+def truthy(value):
+    """ Stringy Truthyness
+    """
+    value=str(value).lower().strip(' ')
+    return value in ['none','false','0','nope','','[]']
+
+
+#
+# MAIN
+#
+def _run(args):
+    conv_layers=eval(conv_layers)
+    batch_norm=truthy(args.batch_norm)
+    test_run=truthy(args.test)
+    if test_run:
+        tsize=TEST_TRAIN_SIZE
+        vsize=TEST_VALID_SIZE
+        bsize=TEST_BATCH_SIZE
+        epochs=TEST_EPOCHS
+        data=PlanetData(train_size=TEST_TRAIN_SIZE)
+    else:
+        tsize=int(args.train_size)
+        vsize=math.floor(train_size*VALID_PCT)
+        bsize=int(args.batch_size)
+        epochs_pct=float(args.epochs_pct)
+        epochs=math.floor(epochs_pct*tsize/bsize)
+        data=PlanetData(train_size='FULL')
+    run(data=data,
+        conv_layers=conv_layers,
+        train_size=tsize,
+        valid_size=vsize,
+        batch_size=bsize,
+        epochs=epochs,
+        batch_norm=batch_norm,
+        weights=args.weights,
+        test_run=test_run)
 
 
 
-run('pre',[3,12],2,5,0.001,5)
-# run('lr02',2,15,0.02,50)
-# run('lr01',2,15,0.01,100)
+def main():
+    parser=argparse.ArgumentParser(description='RUN CONV LAYER TESTS')
+    subparsers=parser.add_subparsers()
+    """ run """
+    parser_run=subparsers.add_parser(
+        'run',
+        help='run aflex based on non default conv_layers')
+    parser_run.add_argument(
+        'conv_layers',help='conv_layers. example: [(32,[3]),(64,[3]),(16,[3])]')
+    parser_run.add_argument(
+        '-s','--size',
+        default=DEFAULT_TRAIN_SIZE,
+        help=f'amount of training data to use. valid_size=floor(size*{VALID_PCT})')
+    parser_run.add_argument(
+        '-b','--batch_size',
+        default=DEFAULT_BATCH_SIZE,
+        help=f'batch_size. default {DEFAULT_BATCH_SIZE})')
+    parser_run.add_argument(
+        '-p','--epochs_pct',
+        default=DEFAULT_EPOCH_PCT,
+        help=f'number of epochs = floor(epochs_pct * TRAIN_SIZE / BATCH SIZE). default {DEFAULT_EPOCH_PCT}')
+    parser_run.add_argument(
+        '-w','--weights',
+        default=None,
+        help=f'path to initial weights')
+    parser_run.add_argument(
+        '-n','--batch_norm',
+        default=DEFAULT_BATCH_NORM,
+        help=f'batch normalize after blocks: True | False. default {DEFAULT_BATCH_NORM}')
+    parser_run.add_argument(
+        '-t','--test',
+        default=DEFUALT_TEST_MODE,
+        help=f'is test run: True | False. default {DEFUALT_TEST_MODE}')
+    parser_run.set_defaults(func=_run)
+    """ init """
+    args=parser.parse_args()
+    args.func(args)
 
 
-
-
-
-
-
-
-
+if __name__ == "__main__": 
+    main()
