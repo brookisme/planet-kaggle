cbb6cb125bd5710fa33c5e5d9b584bda4fb8f6cf
brookie guzder-williams <brook.williams@gmail.com>
2017-07-08 01:25:04 +0000

    tdn_full:1

--- models/graph/tdn_full/model.py
+++ models/graph/tdn_full/model.py
@@ -1,8 +1,15 @@
-graph={
+from keras import regularizers
+from keras import optimizers
+
+l2_decay=1e-3
+
+quick_graph={
     'meta': {
         'network_type': 'TIRAMISU'
     },
-    'compile': True,
+    'compile': {
+        'metrics': ['accuracy']
+    },
     'inputs': {
         'batch_shape': (None,256,256,4),
     },
@@ -12,16 +19,105 @@ graph={
         'padding':'same'
     },
     'down_path':{
-        'layers_list': [1,1,1,1,1]
+        'layers_list': [2,2,2,2,2]
     },
-    'bottleneck': 1,
+    'bottleneck': 2,
     'output_layers': [
         {
             'type':'Flatten'
         },
         { 
             'type':'fc',
-            'units': 2560 
+            'units': 4096
+        },
+        { 
+            'type':'fc',
+            'units': 2048
+        },
+        {
+            'type':'Dense',
+            'units': 17
+        }
+    ],
+    'output': { 
+        'activation': 'sigmoid' 
+    }
+}
+
+graph_b={
+    'meta': {
+        'network_type': 'TIRAMISU'
+    },
+    'compile': {
+        'metrics': ['accuracy']
+    },
+    'inputs': {
+        'batch_shape': (None,256,256,4),
+    },
+    'input_conv': {
+        'filters':48,
+        'kernel_size':3,
+        'padding':'same'
+    },
+    'down_path':{
+        'layers_list': [2,2,2,2,2]
+    },
+    'bottleneck': 4,
+    'output_layers': [
+        {
+            'type':'Flatten'
+        },
+        { 
+            'type':'fc',
+            'units': 4096
+        },
+        { 
+            'type':'fc',
+            'units': 2048
+        },
+        {
+            'type':'Dense',
+            'units': 17
+        }
+    ],
+    'output': { 
+        'activation': 'sigmoid' 
+    }
+}
+
+graph={
+    'meta': {
+        'network_type': 'TIRAMISU'
+    },
+    'compile': {
+        'metrics': ['accuracy'],
+        'keras.optimizers':optimizers.RMSprop(decay=0.995)
+    },
+    'inputs': {
+        'batch_shape': (None,256,256,4),
+    },
+    'input_conv': {
+        'filters':48,
+        'kernel_size':3,
+        'padding':'same'
+    },
+    'down_path':{
+        'layers_list': [4,5,7,10,12]
+    },
+    'bottleneck': 4,
+    'output_layers': [
+        {
+            'type':'Flatten'
+        },
+        { 
+            'type':'fc',
+            'units': 4096,
+            'kernel_regularizer':regularizers.l2(l2_decay)
+        },
+        { 
+            'type':'fc',
+            'units': 2048,
+            'kernel_regularizer':regularizers.l2(l2_decay) 
         },
         {
             'type':'Dense',
